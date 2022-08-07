c09b6a35a8b71422260eccd494407d7a5db3e824
Julian Schenkemeyer <julianschenkemeyer@fastmail.com>
2022-08-07 19:44:29 +0200

    feat(Notification) Add UNUserNotificationCenterDelegate in order to react to notification when the app is in foreground. Also method to prepare for reacting to notification actions.

--- LocalNotificationTest/Shared/LocalNotificationTestApp.swift
+++ LocalNotificationTest/Shared/LocalNotificationTestApp.swift
@@ -6,9 +6,36 @@
 //
 
 import SwiftUI
+import UserNotifications
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+		// Show local notification in foreground
+		UNUserNotificationCenter.current().delegate = self
+		
+		return true
+	}
+}
+// Conform to UNUserNotificationCenterDelegate to show local notification in foreground
+extension AppDelegate: UNUserNotificationCenterDelegate {
+	// Handle actions triggered by the user on the notification
+	func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse) async {
+		print(response)
+	}
+	
+	// Handle notifications received when the app is in foreground
+	func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+		// Show only a badge when the app is in foreground
+		return [.badge]
+	}
+}
 
 @main
 struct LocalNotificationTestApp: App {
+	
+	@UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
     var body: some Scene {
         WindowGroup {
             NotificationOverviewView()
