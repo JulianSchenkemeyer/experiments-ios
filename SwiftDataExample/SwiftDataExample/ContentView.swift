9367073fbd93c40b3f1d3af6298266df2730fcb2
Julian Schenkemeyer <julian@jschenkemeyer.com>
2023-10-05 17:56:20 +0200

    Create SwiftDataExample project

---
+++ SwiftDataExample/SwiftDataExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataExample
+//
+//  Created by Julian Schenkemeyer on 05.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
